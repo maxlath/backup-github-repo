9269a6e0e8d8373d3c883270855c9edd168b2942
maxlath <k@maxlath.eu>
2021-05-04 15:46:08 +0200

    init_config: update token pattern
    fix #7

--- bin/init_config.js
+++ bin/init_config.js
@@ -25,7 +25,7 @@ Where should the config file be stored?
   else return possibleConfigPaths[0]
 }
 
-const tokenPattern = /^[0-9a-f]{40}$/
+const tokenPattern = /^\w{40}$/
 const requestToken = async () => {
   const token = await prompt(`
 
